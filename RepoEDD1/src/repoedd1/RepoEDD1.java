46f20128db9415aa3b387a4fcc9df401039ad956
un1c0 <nestor.pastor.piera@gmail.com>
2016-03-12 13:40:32 +0100

    Pujant canvui 1 Netbeans

--- RepoEDD1/src/repoedd1/RepoEDD1.java
+++ RepoEDD1/src/repoedd1/RepoEDD1.java
@@ -15,7 +15,7 @@ public class RepoEDD1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("IDE Netbeans");
     }
     
 }
