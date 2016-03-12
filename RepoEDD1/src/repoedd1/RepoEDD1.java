2711cf0e981162b5c51f36a15493027cb848f490
un1c0 <nestor.pastor.piera@gmail.com>
2016-03-12 13:34:12 +0100

    Missatge des d´Eclipse

--- RepoEDD1/src/repoedd1/RepoEDD1.java
+++ RepoEDD1/src/repoedd1/RepoEDD1.java
@@ -15,7 +15,8 @@ public class RepoEDD1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("Hello World");
+        System.out.print("IDE Eclipse");
     }
     
 }
