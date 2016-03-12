7673cf405dd1b4706a1061f03ef667162ffd0458
un1c0 <nestor.pastor.piera@gmail.com>
2016-03-12 13:35:27 +0100

    Segon modificació IDE Eclipse

--- RepoEDD1/src/repoedd1/RepoEDD1.java
+++ RepoEDD1/src/repoedd1/RepoEDD1.java
@@ -17,6 +17,8 @@ public class RepoEDD1 {
     public static void main(String[] args) {
         System.out.print("Hello World");
         System.out.print("IDE Eclipse");
+        
+        System.out.print("Inserir una linia mes en main.");
     }
     
 }
