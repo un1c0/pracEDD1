f5666f30676187b085c7a66b9f70c638cda5c49f
un1c0 <nestor.pastor.piera@gmail.com>
2016-03-12 13:24:13 +0100

    Pujant els canvits inicials.

---
+++ RepoEDD1/src/repoedd1/RepoEDD1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repoedd1;
+
+/**
+ *
+ * @author un1c0
+ */
+public class RepoEDD1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
